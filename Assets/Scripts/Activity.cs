aa829186d6f2959d9137b03b178dc5590f98dd97
Eric Cook <jcook83@gatech.edu>
2014-12-01 00:12:25 -0500

    Added in frog instructions and mat instructions

--- Assets/Scripts/Activity.cs
+++ Assets/Scripts/Activity.cs
@@ -14,26 +14,28 @@ public class Activity : MonoBehaviour {
 			loc = GlobeManager.instance.locations.Find(i => i.transform.name.Equals("Africa")).GetComponent<Location>();
 			if(!GlobeManager.instance.isComplete(loc)){
 				GlobeManager.instance.unlockLevel(loc);
+				loc.renderer.material.color = Color.yellow;
 				loc = GlobeManager.instance.locations.Find(i => i.transform.name.Equals("Brazil")).GetComponent<Location>();
 				GlobeManager.instance.unlockLevel(loc);
+				loc.renderer.material.color = Color.yellow;
 				
 				loc = GlobeManager.instance.locations.Find(i => i.transform.name.Equals("Japan")).GetComponent<Location>();
 				GlobeManager.instance.levelComplete(loc);
-				loc.renderer.material.color = Color.yellow;
+				loc.renderer.material.color = Color.green;
 			}
 		}
 		else if(index == 1){
 			loc = GlobeManager.instance.locations.Find(i => i.transform.name.Equals("Africa")).GetComponent<Location>();
 			if(!GlobeManager.instance.isComplete(loc)){
 				GlobeManager.instance.levelComplete(loc);
-				loc.renderer.material.color = Color.yellow;
+				loc.renderer.material.color = Color.green;
 			}
 		}
 		else if (index == 2){
 			loc = GlobeManager.instance.locations.Find(i => i.transform.name.Equals("Brazil")).GetComponent<Location>();
 			if(!GlobeManager.instance.isComplete(loc)){
 				GlobeManager.instance.levelComplete(loc);
-				loc.renderer.material.color = Color.yellow;
+				loc.renderer.material.color = Color.green;
 			}
 		}
 		GameObject.Find("Sphere").GetComponent<SpinGlobe>().enabled = true;
