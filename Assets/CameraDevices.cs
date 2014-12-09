3607067541f3083ab958a53c6cdd8adc8ea01f9f
Eric Cook <jcook83@gatech.edu>
2014-12-09 17:59:44 -0500

    Finding camera names

---
+++ Assets/CameraDevices.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDevices : MonoBehaviour {
+	
+	WebCamTexture frontTex;
+	WebCamTexture backTex;
+	
+	void Start(){
+		WebCamDevice[] devices = WebCamTexture.devices;
+		
+		for(int i = 0 ; i < devices.Length ; i++ ) {
+			if (devices[i].isFrontFacing) {
+				Debug.Log (devices[i].name + " : Front");
+			} else {
+				Debug.Log (devices[i].name + " : Back");
+			}
+		}
+		//frontTex = new WebCamTexture(frontCamName, width, height, framesPerSec);
+		//backTex = new WebCamTexture(backCamName, width, height, framesPerSec);
+	}
+}
