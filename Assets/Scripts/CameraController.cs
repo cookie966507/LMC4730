4e27a2c3962f9d6fb8871a7979cc9cf2f49b2b80
eham30 <eham1632@gmail.com>
2014-12-09 17:38:10 -0500

    Added postcards, but it doesn't work

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	const int CAMERA_RESOLUTION = 256;
+	public string deviceName;
+	public WebCamTexture wct;
+	// Use this for initialization
+	void Start ()
+	{
+		
+		WebCamDevice[] devices = WebCamTexture.devices;
+		deviceName = devices[0].name;
+		wct = new WebCamTexture(deviceName, CAMERA_RESOLUTION, CAMERA_RESOLUTION, 12);
+		renderer.material.mainTexture = wct;
+		wct.Play();
+	}
+
+}
