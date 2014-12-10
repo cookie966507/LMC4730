447dd7c1a629000e7c786496d974e1e722538fed
Eric Cook <jcook83@gatech.edu>
2014-12-10 02:49:00 -0500

    Other two fake activities in

---
+++ Assets/Scripts/Jumping.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumping : MonoBehaviour {
+
+	public GameObject sphere;
+	
+	Vector3 start;
+	
+	int index = 0;
+		
+	public Transform[] locs;
+	
+	void Start(){
+		start = transform.position;
+	}
+	
+	void Update () {
+		if(transform.position.Equals(locs[index].position)){
+			index++;
+		}
+		else{
+			transform.position = Vector3.MoveTowards(transform.position, locs[index].position, 10f);
+		}
+		if(index == 7){
+			Reset();
+		}
+	}
+	
+	void Reset(){
+		transform.position = start;
+		sphere.SetActive (true);
+		sphere.GetComponent<SpinGlobe>().enabled = true;
+		gameObject.transform.root.gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/TimedDelay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDelay : MonoBehaviour {
+	
+	public GameObject sphere;
+	public float delay = 5.0f;
+	private float time = 0;
+	
+	void Update () {
+		time += Time.deltaTime;
+		if(time > delay){
+			time = 0;
+			sphere.SetActive (true);
+			sphere.GetComponent<SpinGlobe>().enabled = true;
+			gameObject.transform.root.gameObject.SetActive(false);
+		}
+	}
+}
